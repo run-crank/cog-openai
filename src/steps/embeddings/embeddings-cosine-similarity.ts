d75b0b3b7b98a5a267fbab0cff40f3eb5fd98378
Sev Geraskin <geraskin@gmail.com>
2023-10-30 19:01:52 -0700

    working cosine similarity step

--- src/steps/embeddings/embeddings-cosine-similarity.ts
+++ src/steps/embeddings/embeddings-cosine-similarity.ts
@@ -6,18 +6,22 @@ import * as util from '@run-crank/utilities';
 import { baseOperators } from '../../client/constants/operators';
 const similarity = require('compute-cosine-similarity');
 
-export class CompletionCosineSimilarity extends BaseStep implements StepInterface {
+export class EmbeddingsCosineSimilarity extends BaseStep implements StepInterface {
 
-  protected stepName: string = 'Check OpenAI GPT cosine similarity of response and provided text embeddings from completion';
+  protected stepName: string = 'Check OpenAI GPT cosine similarity of two texts based on embeddings';
   // tslint:disable-next-line:max-line-length
-  protected stepExpression: string = 'OpenAI model (?<model>[a-zA-Z0-9_-]+) response to (?<prompt>[a-zA-Z0-9_-]+) cosine similarity to (?<comparetext>[a-zA-Z0-9_-]+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<semanticsimilarity>.+)?';
+  protected stepExpression: string = 'OpenAI model (?<model>[a-zA-Z0-9_-]+) cosine similarity of (?<text1>[a-zA-Z0-9_-]+) (?<text2>[a-zA-Z0-9_-]+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<semanticsimilarity>.+)?';
   protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
   protected actionList: string[] = ['check'];
   protected targetObject: string = 'Completion';
   protected expectedFields: Field[] = [{
-    field: 'prompt',
+    field: 'text1',
     type: FieldDefinition.Type.STRING,
-    description: 'User Prompt to send to GPT',
+    description: 'First text to compare',
+  }, {
+    field: 'text2',
+    type: FieldDefinition.Type.STRING,
+    description: 'Second text to compare',
   }, {
     field: 'model',
     type: FieldDefinition.Type.STRING,
@@ -32,11 +36,6 @@ export class CompletionCosineSimilarity extends BaseStep implements StepInterfac
     type: FieldDefinition.Type.NUMERIC,
     description: 'Cosine Similarity Score',
     optionality: FieldDefinition.Optionality.OPTIONAL,
-  }, {
-    field: 'comparetext',
-    type: FieldDefinition.Type.STRING,
-    description: 'Expected text to compare to GPT response',
-    optionality: FieldDefinition.Optionality.OPTIONAL,
   }];
 
   protected expectedRecords: ExpectedRecord[] = [{
@@ -47,55 +46,49 @@ export class CompletionCosineSimilarity extends BaseStep implements StepInterfac
       type: FieldDefinition.Type.STRING,
       description: 'Completion Model',
     }, {
-      field: 'prompt',
-      type: FieldDefinition.Type.STRING,
-      description: 'Completion Prompt',
+      field: 'text1embeddings',
+      type: FieldDefinition.Type.ANYNONSCALAR,
+      description: 'Text 1 Embeddings',
     }, {
-      field: 'compareText',
-      type: FieldDefinition.Type.STRING,
-      description: 'Expected text to compare to GPT response',
-      optionality: FieldDefinition.Optionality.OPTIONAL,
+      field: 'text2embeddings',
+      type: FieldDefinition.Type.ANYNONSCALAR,
+      description: 'Text 2 Embeddings',
     }, {
       field: 'cosineSimilarity',
       type: FieldDefinition.Type.NUMERIC,
       description: 'Cosine Similarity Score',
     }, {
-      field: 'promptUsage',
+      field: 'text1Usage',
       type: FieldDefinition.Type.ANYNONSCALAR,
-      description: 'Prompt API Token Usage',
+      description: 'Text 1 API Token Usage',
     }, {
-      field: 'compareTextUsage',
+      field: 'text2Usage',
       type: FieldDefinition.Type.ANYNONSCALAR,
-      description: 'Comparison Text API Token Usage',
+      description: 'Text 2 API Token Usage',
     }],
     dynamicFields: true,
   }];
 
   async executeStep(step: Step) {
     const stepData: any = step.getData() ? step.getData().toJavaScript() : {};
-    const compareText = stepData.comparetext;
-    const prompt = stepData.prompt;
+    const text1 = stepData.text1;
+    const text2 = stepData.text2;
     const model = stepData.model || 'text-embedding-ada-002';
     const expectedSimilarity = stepData.cosinesimilarity;
     const operator = stepData.operator || 'be';
 
     try {
-      const messages = [];
-      const message = {};
-      message['role'] = 'user';
-      message['content'] = prompt;
-      messages.push(message);
-      const promptEmbeddingsResult = await this.client.getEmbeddings(model, prompt);
-      const compareEmbeddingsResult = await this.client.getEmbeddings(model, compareText);
-      const cosineSimilarity = similarity(promptEmbeddingsResult.data.embedding, compareEmbeddingsResult.data.embedding);
+      const text1EmbeddingsResult = await this.client.getEmbeddings(model, text1);
+      const text2EmbeddingsResult = await this.client.getEmbeddings(model, text2);
+      const cosineSimilarity = similarity(text1EmbeddingsResult.data[0].embedding, text2EmbeddingsResult.data[0].embedding);
       const result = this.assert(operator, cosineSimilarity.toString(), expectedSimilarity.toString(), 'response');
       const returnObj = {
         model,
-        prompt,
-        compareText,
         cosineSimilarity,
-        promptUsage: promptEmbeddingsResult.usage,
-        compareTextUsage: compareEmbeddingsResult.usage,
+        text1Embeddings: text1EmbeddingsResult.data[0].embedding,
+        text2Embeddings: text2EmbeddingsResult.data[0].embedding,
+        text1Usage: text1EmbeddingsResult.usage,
+        text2Usage: text2EmbeddingsResult.usage,
       };
       const records = this.createRecords(returnObj, stepData['__stepOrder']);
       return result.valid ? this.pass(result.message, [], records) : this.fail(result.message, [], records);
@@ -121,4 +114,4 @@ export class CompletionCosineSimilarity extends BaseStep implements StepInterfac
   }
 }
 
-export { CompletionCosineSimilarity as Step };
+export { EmbeddingsCosineSimilarity as Step };
