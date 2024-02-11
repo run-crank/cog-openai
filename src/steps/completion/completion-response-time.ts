b03beab298d679fc4e3fa8a2bbeae4e2a6d5ce67
Kent-Danielle <kconcengco@my.bcit.ca>
2024-02-10 20:08:47 -0800

    Updated readme with response time step description

--- src/steps/completion/completion-response-time.ts
+++ src/steps/completion/completion-response-time.ts
@@ -15,7 +15,7 @@ export class CompletionResponseTime extends BaseStep implements StepInterface {
 
 	// tslint:disable-next-line:max-line-length
 	protected stepExpression: string =
-		'OpenAI model (?<model>[a-zA-Z0-9_-]+) response time in a response to "(?<prompt>[a-zA-Z0-9_ -]+)" should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)? ms';
+		'OpenAI model (?<model>[a-zA-Z0-9_-]+) response time in response to "(?<prompt>[a-zA-Z0-9_ -]+)" should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectation>.+)? ms';
 
 	protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
 
