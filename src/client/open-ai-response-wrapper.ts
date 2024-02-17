8abe42c8f1404c25cdd5bbf6fc35c629f0415d30
Sev Geraskin <geraskin@gmail.com>
2024-02-17 08:44:09 -0800

    fixing eslint errors

--- src/client/open-ai-response-wrapper.ts
+++ src/client/open-ai-response-wrapper.ts
@@ -1,26 +1,25 @@
-import openai from "openai";
-import { ChatCompletion } from "openai/resources";
-
 export class ChatCompletionWrapper<ChatCompletion> {
-	private originalClass: ChatCompletion;
-    public response_time: number;
+  private originalClass: ChatCompletion;
 
-	constructor(originalClass: ChatCompletion, responseTime: number) {
-		this.originalClass = originalClass;
-        this.copyProperties();
-        this.response_time = responseTime;
-	}
+  public response_time: number;
 
-	copyProperties() {
-		for (const prop in this.originalClass) {
-			if (this.originalClass.hasOwnProperty(prop)) {
-				// Use Object.defineProperty to copy property descriptor
-				Object.defineProperty(
-					this,
-					prop,
-					Object.getOwnPropertyDescriptor(this.originalClass, prop)!
-				);
-			}
-		}
-	}
+  constructor(originalClass: ChatCompletion, responseTime: number) {
+    this.originalClass = originalClass;
+    this.copyProperties();
+    this.response_time = responseTime;
+  }
+
+  copyProperties() {
+    for (const prop in this.originalClass) {
+      // Using Object.prototype.hasOwnProperty.call() for safer property check
+      if (Object.prototype.hasOwnProperty.call(this.originalClass, prop)) {
+        // Use Object.defineProperty to copy property descriptor
+        Object.defineProperty(
+          this,
+          prop,
+          Object.getOwnPropertyDescriptor(this.originalClass, prop)!,
+        );
+      }
+    }
+  }
 }
